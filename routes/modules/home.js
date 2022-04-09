1e9b2193f1faaa62d119c4b055daab6a1767fe38
Tommy1023 <j.h.chen1023@gmail.com>
2022-04-10 01:33:57 +0800

    feat: modify index.hbs & home.js

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,12 +2,19 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const category = require('../../models/category')
+const { render } = require('express/lib/response')
 
 router.get('/', (req, res) => {
-  let renderRecords = []
-  Record.find()
+  Category.find()
+  .lean()
+  .then(categories => {
+    Record.find()
     .lean()
     .then(records => {
+      let renderRecords = []
+      let totalAmount = 0
+      records.forEach(record => totalAmount += record.amount)
       return Promise.all(Array.from(records, record => {
         return Category.findOne({ _id: record.categoryId })
         .lean()
@@ -17,11 +24,37 @@ router.get('/', (req, res) => {
         })
       }))
       .then(() => {
-        res.render('index', { renderRecords })})
+        res.render('index', { categories, renderRecords, totalAmount })})
+      })
     })
     .catch(err => console.log('some error', err))
 })
 
+
+router.get('/sort/:sort', (req, res) => {
+  const sort =req.params.sort
+  Category.find()
+    .lean()
+    .then(categories => {
+      let selectedCategory = {}
+      categories.map((data) => {
+        if (data.name === sort) {
+          return selectedCategory = data
+        }
+      })
+      Record.find({ categoryId: selectedCategory._id })
+        .lean()
+        .then(renderRecords => {
+          let totalAmount = 0
+          renderRecords.forEach(record => {
+            totalAmount += record.amount
+            record.icon = selectedCategory.icon
+          })
+          res.render('index', { categories, renderRecords, totalAmount, sort })
+        })
+    })
+    .catch(err => console.log('some error', err))
+})
 
 
 module.exports = router
