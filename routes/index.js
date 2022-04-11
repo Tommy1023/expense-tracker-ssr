e445d5008b77090c4f2ec8896fd5ada7b8d19e2a
Tommy1023 <j.h.chen1023@gmail.com>
2022-04-12 03:07:24 +0800

    feat:add facebook login

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const home = require('../routes/modules/home')
 const records = require('../routes/modules/records')
 const users = require('../routes/modules/users')
 const { authenticator } = require('../middleware/auth')
-
+const auth = require('../routes/modules/auth')
 
 router.use('/users', users)
 router.use('/records',authenticator, records)
+router.use('/auth', auth)
 router.use('/',authenticator, home)
 
 module.exports = router
