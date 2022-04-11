e5e154b0c1dc27a6585ba86d0533b828655f4a31
Tommy1023 <j.h.chen1023@gmail.com>
2022-04-12 00:30:53 +0800

    feat: add users.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const home = require('../routes/modules/home')
 const records = require('../routes/modules/records')
+const users = require('../routes/modules/users')
 
-
+router.use('/users', users)
 router.use('/records', records)
 router.use('/', home)
 
---
+++ routes/modules/users.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const User = require('../../models/user')
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不符!' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+  User.findOne({ email }).then(user => {
+    if (user) {
+      errors.push({ message: '這個 Email 已被註冊過!' })
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => {
+        // req.logout()
+        res.redirect('/users/login')
+      })
+      .catch(err => console.log('some error:', err))
+  })
+    .catch(err => console.log('some error:', err))
+})
+
+
+
+module.exports = router
